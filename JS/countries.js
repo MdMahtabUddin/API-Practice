6fd956b01543c8c508a9961d0b65dd3d88b3821a
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-11-03 11:40:33 +0000

    edit

--- JS/countries.js
+++ JS/countries.js
@@ -1,7 +1,19 @@
 const loadCountries =()=>{
-    fetch('https://restcountries.com/v3.1/all')
+    fetch('https://restcountries.com/v2/all')
     .then(res=>res.json())
     .then(data=>console.log(data))
 
 }
 loadCountries()
+
+const displayCountries =country =>{
+    // for(const countries of country){
+    //     console.log(countries)
+    // }
+const countryDiv =
+    country.forEach(countries=>{
+       const h3 =document.createElement('h3')
+       h3.innerText =countries.name
+
+    })
+}
